50489d3c5ae08d91d247cb2db318416307254315
Alan Henry <alandavidhenry@gmail.com>
2024-08-02 18:42:43 +0100

    feat: add tailwind css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/scss')
+  eleventyConfig.addPassthroughCopy('src/js')
+
   return {
     dir: {
       input: 'src',
