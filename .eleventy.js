944f13418e0aece89eaf3e879fcc51b08f2cab59
Alan Henry <alandavidhenry@gmail.com>
2024-08-02 18:58:08 +0100

    feat: add browser sync

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/scss')
   eleventyConfig.addPassthroughCopy('src/js')
 
+  eleventyConfig.addWatchTarget('./src/css/')
+
   return {
     dir: {
       input: 'src',
