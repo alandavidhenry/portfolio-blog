f468e4d984f49c27a5c50c32b5d2efef70fa25a3
Alan Henry <alandavidhenry@gmail.com>
2024-08-02 19:47:32 +0100

    fix: add passthrough for decap cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/scss')
   eleventyConfig.addPassthroughCopy('src/js')
+  eleventyConfig.addPassthroughCopy({ 'src/public': '/' })
+  eleventyConfig.addPassthroughCopy('./src/admin')
+  eleventyConfig.addPassthroughCopy('./src/posts')
 
   eleventyConfig.addWatchTarget('./src/css/')
 
