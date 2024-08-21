17ed5fb6a1d334640b8132f8a22e57d39af2c939
Alan Henry <alandavidhenry@gmail.com>
2024-08-21 16:52:53 +0100

    add cv

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' })
   eleventyConfig.addPassthroughCopy('./src/admin')
   eleventyConfig.addPassthroughCopy('./src/posts')
+  eleventyConfig.addPassthroughCopy('./src/public')
 
   eleventyConfig.addWatchTarget('./src/css/')
 
