ef161c8ed93feef9aba3ef5724a5d7008b45780d
Alan Henry <alandavidhenry@gmail.com>
2024-09-23 10:35:56 +0100

    feat: add images to portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
       const fullDescription = card.querySelector('.full-description')
       const url = card.querySelector('.url')
       const github = card.querySelector('.github')
+      const more = card.querySelector('.more')
 
       description.classList.add('hidden')
       fullDescription.classList.remove('hidden')
       url.classList.remove('hidden')
       github.classList.remove('hidden')
+      more.classList.add('hidden')
       card.dataset.expanded = 'true'
     })
   })
